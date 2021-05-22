a3313cffb21df5065ccae7bee049b7d702d73b58
Prashant Kumar <77384223+PrAsH-01@users.noreply.github.com>
2021-05-22 18:43:08 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+  function calculateTip()
+{
+     var billamt = document.getElementById("billamt").value;
+     var serviceQual = document.getElementById("serviceQual").value;
+     var intrate = document.getElementById("intrate").value;
+    
+    
+    if(billamt==="" || serviceQual==0 || billamt==0 )
+        {
+            alert("please enter values");
+            reture;
+        }
+    if(billamt==0 || billamt<0 )
+        {
+            alert("please enter positive no.");
+            reture;
+        }
+    
+    else
+    {
+        document.getElementById("each").style.display="block"; 
+    }
+    var total=(billamt*serviceQual*intrate)/100;
+    //total=Math.round(total);
+     document.getElementById("totalTip").style.display="block";
+     document.getElementById("tip").innerHTML=total;
+}
+
+document.getElementById("totalTip").style.display="none";
+document.getElementById("totalTip").style.display="none";
+
+document.getElementById("calculate").onclick=function()
+{
+    calculateTip();
+}
